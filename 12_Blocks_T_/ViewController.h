2a03be7563f2778623961209485aed213e7d54d1
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-02-03 08:55:11 +0200

    Initial Commit

---
+++ 12_Blocks_T_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  12_Blocks_T_
+//
+//  Created by Stas on 03.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 12_Blocks_T_/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  12_Blocks_T_
+//
+//  Created by Stas on 03.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
